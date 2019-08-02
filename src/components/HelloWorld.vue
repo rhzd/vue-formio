c5d0233da273798fef7f0476091a46270125f22a
rhzd <i.ruhaizad@gmail.com>
2019-08-02 14:05:24 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <formio ref="formio" src="http://192.168.2.222:3001/user/register"></formio>
+  </div>
+</template>
+
+<script>
+  import { Form } from 'vue-formio';
+  export default {
+      name: 'app',
+      components: { formio: Form },
+      data(){
+        return{
+
+        }
+      },
+      mounted(){
+        console.log(this.$refs.formio);
+      }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <formio ref="formio" src="http://192.168.2.222:3001/contact"></formio>
+</template>
+
+<script>
+  import { Form } from 'vue-formio';
+  export default {
+      name: 'app',
+      components: {
+        formio: Form
+       },
+      data(){
+        return{
+
+        }
+      },
+      mounted(){
+        console.log(this.$refs.formio);
+      }
+  }
+</script>
